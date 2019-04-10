4d87ea916169508d653e88426a8d4c897fa20ae7
marcomontoya <marcoamontoyam@gmail.com>
2019-04-10 02:51:30 -0500

    adding css and js files

--- lineasproducto.php
+++ lineasproducto.php
@@ -1,68 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Lineas de producto</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css" />
   <link rel="stylesheet" href="edittable.css"> 
-  <script type="text/javascript">
-    var $TABLE = $('#table');
-    var $BTN = $('#export-btn');
-    var $EXPORT = $('#export');
-
-    $('.table-add').click(function () {
-      var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
-      $TABLE.find('table').append($clone);
-    });
-
-    $('.table-remove').click(function () {
-      $(this).parents('tr').detach();
-    });
-
-    $('.table-up').click(function () {
-      var $row = $(this).parents('tr');
-      if ($row.index() === 1) return; // Don't go above the header
-      $row.prev().before($row.get(0));
-    });
-
-    $('.table-down').click(function () {
-      var $row = $(this).parents('tr');
-      $row.next().after($row.get(0));
-    });
-
-    // A few jQuery helpers for exporting only
-    jQuery.fn.pop = [].pop;
-    jQuery.fn.shift = [].shift;
-
-    $BTN.click(function () {
-      var $rows = $TABLE.find('tr:not(:hidden)');
-      var headers = [];
-      var data = [];
-      
-      // Get the headers (add special header logic here)
-      $($rows.shift()).find('th:not(:empty)').each(function () {
-        headers.push($(this).text().toLowerCase());
-      });
-      
-      // Turn all existing rows into a loopable array
-      $rows.each(function () {
-        var $td = $(this).find('td');
-        var h = {};
-        
-        // Use the headers from earlier to name our hash keys
-        headers.forEach(function (header, i) {
-          h[header] = $td.eq(i).text();   
-        });
-        
-        data.push(h);
-      });
-      
-      // Output the result
-      $EXPORT.text(JSON.stringify(data));
-    });
-  </script>
+  
 </head>
 <body>
 <?php
